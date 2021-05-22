12549d95221620bdb2fa249ac5c8b4217b524651
Josh Aragon <josharagon99@gmail.com>
2021-05-21 23:13:28 -0600

    Add basic card format

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,20 @@
+import './Card.css'
+
+
+function Card() {
+
+  return (
+    <div className='card'>
+      <div className='top-left'>
+        <p>4</p>
+        <p>❤️</p>
+      </div>
+      <div className='bottom-right'>
+        <p>4</p>
+        <p>❤️</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
