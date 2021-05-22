4f16e2cc586921219aef33958b98b7a35b743d01
Josh Aragon <josharagon99@gmail.com>
2021-05-22 01:48:05 -0600

    Add shuffle button that changes order of all cards

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,40 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import { useState, useEffect, Component } from 'react';
 import Deck from '../Deck/Deck.js';
-import Card from '../Card/Card.js';
 import cardData from '../../cardData.js';
 import createAllCards from '../../createAllCards'
 
-function App() {
-  const [cards, setCards] = useState([])
-  
-console.log(    createAllCards(cardData))
-  return(
-    <main>
-      <Deck cards={cards}/>
-    </main>
-  )
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      cards:[]
+    }
+  }
+
+  componentDidMount() {
+    this.setState({cards: createAllCards(cardData)})
+  }
+
+  shuffleCards(cardArray) {
+    for (let i = cardArray.length - 1; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * i);
+      let temp = cardArray[i];
+      cardArray[i] = cardArray[randomIndex];
+      cardArray[randomIndex] = temp;
+    }
+    this.setState({cards: cardArray})
+  }
+
+  render() {
+    return (
+      <main>
+        <button onClick={() => this.shuffleCards(this.state.cards)}>Shuffle</button>
+        <Deck cards={this.state.cards} />
+      </main>
+    )
+  }
 }
 
 export default App;
--- src/Components/Deck/Deck.js
+++ src/Components/Deck/Deck.js
@@ -1,9 +1,23 @@
 import Card from '../Card/Card.js';
 
 
-function Deck() {
+function Deck({cards}) {
+
+  const allCards = cards.map(card => {
+    return (
+      <Card 
+        value={card.value}
+        suit={card.suit}
+        color={card.color}
+        key={card.value+card.suit}
+      />
+    )
+  })
+
   return (
-    <div></div>
+    <>
+    {allCards}
+    </>
   )
 }
 
